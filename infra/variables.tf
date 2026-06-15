1982fab2b5ee45b215daf2e7087d633d2cb4fe19
SebastianAlecio <sebastianalecio@gmail.com>
2026-06-15 17:29:18 -0600

    [oyd-d4] limpiar comentario residual del demo BROKEN

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
   }
 }
 
-variable "project_name" { # BROKEN: extra spaces, demo ruleset blocking
+variable "project_name" {
   description = "Short project identifier used as a name component and in default tags."
   type        = string
   default     = "pdds-oyd"
